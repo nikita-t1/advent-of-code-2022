5dc1ad34ceb01b30e800d72467df002369b27a21
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-22 12:54:46 +0100

    Day 02 - Part 2
    Code is a mess - I'm not proud of it

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,9 +1,9 @@
 import kotlin.system.exitProcess
 
-enum class RockPaperScissors(val letter: String, val score: Int, private val beatenBy: Int){
-    ROCK("A", 1, 2),
-    PAPER("B", 2, 3),
-    SCISSORS("C", 3, 1);
+enum class RockPaperScissors(val letter: String, val score: Int, val beatenBy: Int, val beats: Int){
+    ROCK("A", 1, 2, 3),
+    PAPER("B", 2, 3, 1),
+    SCISSORS("C", 3, 1, 2);
 
     fun score(opponent: Int): Int {
         if (this.score == opponent) { // Draw
@@ -12,6 +12,20 @@ enum class RockPaperScissors(val letter: String, val score: Int, private val bea
             return if (this.beatenBy == opponent) 0 else 6
         }
 
+    }
+}
+
+enum class Outcome(val letter: String){
+    LOSE("X"),
+    DRAW("Y"),
+    WIN("Z");
+
+    fun neededScore(): Int{
+        return when(this){
+            LOSE -> 0
+            DRAW -> 3
+            WIN -> 6
+        }
     }
 }
 
@@ -44,8 +58,43 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
-    }
+
+        var totalScore = 0
+        input.forEach { line ->
+            run {
+                val opponent = when(line.substringBefore(" ")) {
+                    "A" -> RockPaperScissors.ROCK
+                    "B" -> RockPaperScissors.PAPER
+                    "C" -> RockPaperScissors.SCISSORS
+                    else -> RockPaperScissors.ROCK
+                }
+                val neededOutcome = when(line.substringAfter(" ")) {
+                    "X" -> Outcome.LOSE
+                    "Y" -> Outcome.DRAW
+                    "Z" -> Outcome.WIN
+                    else -> Outcome.LOSE
+                }
+
+                val myInt = when(neededOutcome) {
+                    Outcome.LOSE -> opponent.beats
+                    Outcome.DRAW -> opponent.score
+                    Outcome.WIN -> opponent.beatenBy
+                }
+
+                val me = when(myInt) {
+                    1 -> RockPaperScissors.ROCK
+                    2 -> RockPaperScissors.PAPER
+                    3 -> RockPaperScissors.SCISSORS
+                    else -> {RockPaperScissors.ROCK}
+                }
+
+                val shapeScore : Int = me.score
+
+                val outcome = me.score(opponent.score)
+                totalScore += (outcome + shapeScore)
+            }
+        }
+        return totalScore    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
