5161d317c31541610a18716976cb58d8157d3620
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-22 21:04:33 +0100

    Day 03 - Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,9 +1,15 @@
 
 fun main() {
 
-    fun part1(input: List<Pair<List<Char>, List<Char>>>): Int {
+    fun part1(input: List<String>): Int {
+        val list = input.map { line ->
+            Pair(
+                line.substring(0, line.length / 2).map { it },
+                line.substring(line.length / 2).map { it }
+            )
+        }
         var sum = 0
-        input.forEach { pair ->
+        list.forEach { pair ->
             val firstCompartment = pair.first
             val secondCompartment = pair.second
             val inBoth = firstCompartment.find { it in secondCompartment }!!
@@ -14,24 +20,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+
+        var prioritySum = 0
+
+        val currentSet: MutableList<String> = mutableListOf()
+        input.forEachIndexed { index, item ->
+            run {
+                if (((index + 1) % 3) == 0) {
+                    currentSet.add(item)
+
+                    val first: List<Char> = currentSet[0].map { it }
+                    val second: List<Char> = currentSet[1].map { it }
+                    val third: List<Char> = currentSet[2].map { it }
+                    val match = first.find { it in second && it in third }!!
+                    val priority = if (match.isLowerCase()) match.code - 96 else match.code - 38
+                    prioritySum += priority
+
+                    currentSet.clear()
+                } else {
+                    currentSet.add(item)
+                }
+            }
+        }
+        return prioritySum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day03_test").map { line ->
-        Pair(
-            line.substring(0, line.length/2).map { it },
-            line.substring(line.length/2).map { it }
-        )
-    }
+    val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
 
-    val input = readInput("Day03").map { line ->
-        Pair(
-            line.substring(0, line.length/2).map { it },
-            line.substring(line.length/2).map { it }
-        )
-    }
+    val input = readInput("Day03")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
