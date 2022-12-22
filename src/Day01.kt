ffc1b5994b8cb3d9c9d8dfbc04b9377e436fb436
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-22 11:18:17 +0100

    Day 01 - Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val caloriesList = mutableListOf<Int>()
+        var currentElfCalories = 0
+        input.forEach { line ->
+            run {
+                if (line.isBlank()) {
+                    caloriesList.add(currentElfCalories)
+                    currentElfCalories = 0
+                } else {
+                    currentElfCalories += line.toInt()
+                }
+            }
+        }
+        return caloriesList.maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
