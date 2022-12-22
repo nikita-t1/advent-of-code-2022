db3480cbb5f14c7832a4a9d0791aaecf7498a583
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-23 00:23:01 +0100

    Day 04 - Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -27,12 +27,38 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var count = 0
+        val pairs = input.map { line ->
+            val firstPairFirst = line.substringBefore(",").substringBefore("-").toInt()
+            val firstPairSecond = line.substringBefore(",").substringAfter("-").toInt()
+            val secondPairFirst = line.substringAfter(",").substringBefore("-").toInt()
+            val secondPairSecond = line.substringAfter(",").substringAfter("-").toInt()
+
+            val firstRange = {
+                val list = mutableListOf<Int>()
+                for (i in firstPairFirst..firstPairSecond){
+                    list.add(i)
+                }
+                list
+            }
+            val secondRange = {
+                val list = mutableListOf<Int>()
+                for (i in secondPairFirst..secondPairSecond){
+                    list.add(i)
+                }
+                list
+            }
+            if (firstRange().any { it in secondRange() }) {
+                count++
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     part1(input).println()
