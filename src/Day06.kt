14c04caef7b51c560487828d04c08701a6eefdfd
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-23 14:35:47 +0100

    Day 06 - Part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,14 +1,18 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        for (i in 0..input.size-4) {
-            val areAllDifferent = input.subList(i, i+4)
-            if (areAllDifferent.distinct().size == 4) return i+4
+        for (i in 0..input.size - 4) {
+            val areAllDifferent = input.subList(i, i + 4)
+            if (areAllDifferent.distinct().size == 4) return i + 4
         }
         return 0
     }
 
     fun part2(input: List<String>): Int {
+        for (i in 0..input.size - 14) {
+            val areAllDifferent = input.subList(i, i + 14)
+            if (areAllDifferent.distinct().size == 14) return i + 14
+        }
         return 0
     }
 
@@ -18,6 +22,11 @@ fun main() {
     check(part1(testInput[1].toList().map { it.toString() }) == 6)
     check(part1(testInput[2].toList().map { it.toString() }) == 10)
     check(part1(testInput[3].toList().map { it.toString() }) == 11)
+
+    check(part2(testInput[0].toList().map { it.toString() }) == 23)
+    check(part2(testInput[1].toList().map { it.toString() }) == 23)
+    check(part2(testInput[2].toList().map { it.toString() }) == 29)
+    check(part2(testInput[3].toList().map { it.toString() }) == 26)
 
     val input = readInput("Day06")[0].toList().map { it.toString() }
     part1(input).println()
